479599c18e1d4edf8595c51b3021a6c184bdd50e
l1sac <meihuan.chen@education.nsw.gov.au>
2024-11-02 17:14:54 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function loadM() {
     document.getElementById('mercury').style.display = "block";
 }
+function loadV() {
+    document.getElementById('venus').style.display = "block";
+}
+function loadE() {
+    document.getElementById('earth').style.display = "block";
+}
