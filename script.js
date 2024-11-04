f59128c5149685d21044cae57a84cd8b18713808
l1sac <meihuan.chen@education.nsw.gov.au>
2024-11-04 15:10:07 +1100

    Update script.js
    - fixed picture inability to come forward

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function loadM() {
     document.getElementById('mercury').style.display = "block";
+    document.getElementById('venus').style.display = "none";
+    document.getElementById('earth').style.display = "none";
 }
 function loadV() {
     document.getElementById('venus').style.display = "block";
+    document.getElementById('mercury').style.display = "none";
+    document.getElementById('earth').style.display = "none";
 }
 function loadE() {
     document.getElementById('earth').style.display = "block";
+    document.getElementById('mercury').style.display = "none";
+    document.getElementById('venus').style.display = "none";
 }
