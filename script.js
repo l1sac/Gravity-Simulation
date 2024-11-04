a9a2621be8465585d472d7f08d98d10be540ed42
l1sac <meihuan.chen@education.nsw.gov.au>
2024-11-04 18:42:02 +1100

    Update script.js
    - feather and brick

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ function loadM() {
     document.getElementById('mercury').style.display = "block";
     document.getElementById('venus').style.display = "none";
     document.getElementById('earth').style.display = "none";
+    document.getElementById('brick').style.display = "block";
+    document.getElementById('feather').style.display = "block";
 }
 function loadV() {
     document.getElementById('venus').style.display = "block";
     document.getElementById('mercury').style.display = "none";
     document.getElementById('earth').style.display = "none";
+    document.getElementById('brick').style.display = "block";
+    document.getElementById('feather').style.display = "block";
 }
 function loadE() {
     document.getElementById('earth').style.display = "block";
     document.getElementById('mercury').style.display = "none";
     document.getElementById('venus').style.display = "none";
+    document.getElementById('brick').style.display = "block";
+    document.getElementById('feather').style.display = "block";
+    
 }
